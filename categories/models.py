a00c18c2d4b09a3646f90a856112b9f053acc73d
Atziri Cruz <127270652+AtziriCruz@users.noreply.github.com>
2024-05-17 22:28:03 -0600

    Update models.py

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Modelo de Categoría para organizar productos
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)  # Nombre de la categoría
+
+    def __str__(self):
+        return self.name  # Devuelve el nombre de la categoría como representación legible del objeto
