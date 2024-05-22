81a272a9a9f88a38f689d63c263edc2455dd7a3a
Rodolfo Domínguez <rodolfodom7@gmail.com>
2024-05-22 00:10:28 -0600

    Delete backend directory
    cambio de proyecto

--- backend/categories/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Modelo de Categoría para organizar productos
-class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)  # Nombre de la categoría
-
-    def __str__(self):
-        return self.name  # Devuelve el nombre de la categoría como representación legible del objeto
